f0b68695d9842934f919b687ef19117e37354ff4
Arun-kumar-19 <118425866+Arun-kumar-19@users.noreply.github.com>
2023-07-18 17:23:37 +0530

    day07
    Solved: 4 problems
    Practice: 2 problems

--- src/main/java/day06/practice/Task.java
+++ src/main/java/day06/practice/Task.java
@@ -1,15 +1,54 @@
 package day06.practice;
 
+import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
 public class Task {
-	
+
 	private String taskName;
-    
-    private int priority;
-    
-    public Task(String name,int priority) {
-    	this.setTaskName(name);
-    	this.setPriority(priority);
-    }
+	private int priority;
+	private int id;
+	private LocalDate deadline;
+
+	public Task(String name, int id, LocalDate date) {
+		this.taskName = name;
+		this.id = id;
+		this.deadline = date;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Task task = (Task) o;
+		return Objects.equals(taskName, task.getTaskName()) && Objects.equals(deadline, task.getDeadline());
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getDeadline() {
+
+		return deadline;
+	}
+
+	public void setDeadline(LocalDate deadline) {
+		this.deadline = deadline;
+	}
+
+	public Task(String name, int priority) {
+		this.setTaskName(name);
+		this.setPriority(priority);
+	}
 
 	public int getPriority() {
 		return priority;
@@ -26,5 +65,5 @@ public class Task {
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
 	}
-	
+
 }
